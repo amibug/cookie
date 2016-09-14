86b32f50f9c6c344d90fbd1c0c397a25f4824660
evan.wyd <evan.wyd@alibaba-inc.com>
2016-09-14 10:04:20 +0800

    用F2ETest的webdriver云做跨浏览器单元测试

--- karma.conf.js
+++ karma.conf.js
@@ -1,100 +1,111 @@
 // Karma configuration
 // Generated on Mon Sep 12 2016 15:35:05 GMT+0800 (CST)
 
-module.exports = function(config) {
-  config.set({
+// Karma configuration
+function getLocalIP() {
+	var ifaces = require('os').networkInterfaces();
+	for (var dev in ifaces) {
+		var iface = ifaces[dev];
+		for (var i = 0; i < iface.length; i++) {
+			var alias = iface[i];
+			if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+				return alias.address;
+			}
+		}
+	}
+}
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: './',
+function createWebdriver(extraConfig) {
+	return Object.assign({
+		'name': 'Karma',
+		'base': 'WebDriver',
+		'config': {
+			'host': 'f2etest.alibaba-inc.com',
+			'port': 4444
+		},
+		// f2etest获取账号和apiKey
+		'f2etest.userid': '',
+		'f2etest.apiKey': '',
+		'pseudoActivityInterval': 30000
+	}, extraConfig);
+}
 
+module.exports = function (config) {
+	config.set({
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: [ 'mocha', 'chai', 'commonjs' ],
+		basePath: './',
 
+		// Browser support Same as Chai.js: IE 9+, Chrome 7+, FireFox 4+, Safari 5+ except should style that is currently not compatible with IE 9.
+		// Consider karma-expect, if you need run tests in IE8 and lower.
+		frameworks: ['mocha', 'chai', 'commonjs'],
 
-    // list of files / patterns to load in the browser
-    files: [
-      //'src/**/*.js',
-      //'test/**/*.js',
-      'src/**/*.js',
-      //'dist/**/*.js',
-      'test/**/*.js'
-    ],
+		files: [
+			'src/**/*.js',
+			'test/**/*.js'
+		],
 
+		exclude: [
+			'karma.conf.js'
+		],
 
-    // list of files to exclude
-    exclude: [
-      'karma.conf.js'
-    ],
+		preprocessors: {
+			'src/**/*.js': ['commonjs', 'coverage'],
+			'test/**/*.js': ['commonjs']
+		},
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      //'src/**/*.js': [ 'babel', 'commonjs' ],
-      //'test/**/*.js': [ 'babel', 'commonjs' ]
-      'src/**/*.js': ['commonjs', 'coverage'],
-      //'dist/**/*.js': ['commonjs'],
-      'test/**/*.js': ['commonjs']
-      //'dist/**/*.js': ['coverage']
-    },
+		reporters: ['progress', 'mocha', 'coverage'],
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'mocha', 'coverage'],
+		port: 9876,
 
+		colors: true,
 
-    // web server port
-    port: 19876,
+		logLevel: config.LOG_INFO,
 
+		autoWatch: false,
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+		//browsers: ['Chrome', 'Firefox', 'Safari', 'PhantomJS', 'Opera', 'IE'],
+		browsers: ['PhantomJS'],
 
+		// Continuous Integration mode
+		// if true, Karma captures browsers, runs the tests and exits
+		singleRun: true,
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+		// Concurrency level
+		// how many browser should be started simultaneous
+		concurrency: Infinity,
 
+		plugins: [
+			'karma-mocha',
+			'karma-chai',
+			'karma-coverage',
+			'karma-mocha-reporter',
+			'karma-phantomjs-launcher',
+			'karma-chrome-launcher',
+			'karma-firefox-launcher',
+			'karma-safari-launcher',
+			'karma-safari-launcher',
+			'karma-ie-launcher',
+			'karma-commonjs',
+			'karma-webdriver-launcher'
+			//'karma-babel-preprocessor',
+		],
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+		coverageReporter: {
+			type: 'html',
+			dir: 'coverage/'
+		},
 
+		//默认hostname是localhost，这个host在本地运行测试没问题
+		//但是远端浏览器无法访问，所以需要在此处配置一个f2etest的浏览器
+		hostname: getLocalIP(),
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    //browsers: ['Chrome', 'Firefox', 'Safari', 'PhantomJS', 'Opera', 'IE'],
-    browsers: ['PhantomJS'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity,
-
-    plugins: [
-      'karma-mocha',
-      'karma-chai',
-      'karma-coverage',
-      'karma-mocha-reporter',
-      'karma-phantomjs-launcher',
-      'karma-chrome-launcher',
-      'karma-firefox-launcher',
-      'karma-safari-launcher',
-      'karma-safari-launcher',
-      'karma-ie-launcher',
-      'karma-commonjs',
-      //'karma-babel-preprocessor',
-    ],
-
-    coverageReporter: {
-      type : 'html',
-      dir: 'coverage/'
-    }
-
-  })
+		customLaunchers: {
+			'f2etest-chrome': createWebdriver({browserName: 'chrome'}),
+			'f2etest-firefox': createWebdriver({browserName: 'firefox'}),
+			'f2etest-ie8': createWebdriver({browserName: 'internet explorer', version: '8'}),
+			'f2etest-ie9': createWebdriver({browserName: 'internet explorer', version: '9'}),
+			'f2etest-ie10': createWebdriver({browserName: 'internet explorer', version: '10'}),
+			'f2etest-ie11': createWebdriver({browserName: 'internet explorer', version: '11'})
+		}
+	})
 }
